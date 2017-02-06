c66b5521a8f672b04f2eec547b0f820f543a911c
Serah Huynh <yuffieh@Yuffiehs-Computer.local>
2017-02-06 14:47:22 -0700

    updated assignment page

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.get("/assignment1/contact", function(req, res){
 	res.sendFile(__dirname + "/joey/assignment-1/contact.html")
 });
 
+app.get("/assignment1/info", function(req, res){
+	res.sendFile(__dirname + "/joey/assignment-1/info.html")
+});
 
 
 app.use(express.static("public"));
